e2c1a998d1bb11bb499b22234093035f3b58da6d
Migliorin Francesco Antonio <28485612+Giglium@users.noreply.github.com>
2021-03-30 15:59:16 +0200

    One Terraform to rule them all (#3)
    1. Move all the `.tf` file under the `terraform` folder;
    2. Update Argo-CD to the latest version, retrieve its password from the secret, and generate Argo-CD Token with Terraform;
    3. Finish to integrate the Kerberus dashboard into Terraform;
    4. Update Dockerfile;
    5. Create GitHub actions to validate terraform and push the docker image to GitHub registry;
    6. [BUG_FIX] Add temporally fix for Crossplane on RKE while we wait for PR: https://github.com/crossplane/crossplane/pull/2240/commits/f630b80dd82c4bbef6c74d3e26861598f4666c2e;
    7. [BUG_FIX] Kubectl on local-exec now use the given `kubeconfig` file.

--- output.tf
+++
@@ -1,5 +0,0 @@
-
-output "argocd" {
-  description = "Argo LINK"
-  value       = "https://${var.ARGOCD_HOSTNAME}"
-}
---
+++ terraform/files/argocd/generate-token.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+test -f $(which jq) || error_exit "jq command not detected in path, please install it"
+
+eval "$(jq -r '@sh "export ARGO_ADMIN_PASSWORD=\(.argo_password) ARGO_HOSTNAME=\(.argo_hostname)"')"
+
+ARGO_AUTH_BEARER_TOKEN=$(curl -k --location --request POST "https://$ARGO_HOSTNAME/api/v1/session" \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "username": "admin",
+    "password": "'"$ARGO_ADMIN_PASSWORD"'"
+}' 2>/dev/null | jq -r '.token')
+
+ARGO_SERVICE_ACCOUNT_TOKEN=$(curl -k --location --request POST "https://$ARGO_HOSTNAME/api/v1/account/kerberus-dashboard/token" \
+--header "Authorization: Bearer $ARGO_AUTH_BEARER_TOKEN" 2>/dev/null | jq -r '.token')
+
+jq -n --arg ARGO_SERVICE_ACCOUNT_TOKEN "$ARGO_SERVICE_ACCOUNT_TOKEN" '{"argo_token":$ARGO_SERVICE_ACCOUNT_TOKEN}'
