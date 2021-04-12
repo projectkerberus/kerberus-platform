d888a2a5f892aabdb5d00e040dee3d34a3d22181
Giglium <fre095@gmail.com>
2021-04-12 16:19:14 +0200

    Curl call to ArgoCD now correctly fail on Terraform

--- terraform/files/argocd/generate-token.sh
+++ terraform/files/argocd/generate-token.sh
@@ -6,14 +6,20 @@ test -f $(which jq) || error_exit "jq command not detected in path, please insta
 
 eval "$(jq -r '@sh "export ARGO_ADMIN_PASSWORD=\(.argo_password) ARGO_HOSTNAME=\(.argo_hostname)"')"
 
-ARGO_AUTH_BEARER_TOKEN=$(curl -k --location --fail --request POST "$ARGO_HOSTNAME/api/v1/session" \
+ARGO_REPONSE=$(curl -k --fail --location --request POST "$ARGO_HOSTNAME/api/v1/session" \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "username": "admin",
     "password": "'"$ARGO_ADMIN_PASSWORD"'"
-}' 2>/dev/null | jq -r '.token')
+}') || \
+(echo "Fail to retreive bearer token. Please check if $ARGO_HOSTNAME is a valid endpoint" >&2; exit 1)
 
-ARGO_SERVICE_ACCOUNT_TOKEN=$(curl -k --location --fail --request POST "$ARGO_HOSTNAME/api/v1/account/kerberus-dashboard/token" \
---header "Authorization: Bearer $ARGO_AUTH_BEARER_TOKEN" 2>/dev/null | jq -r '.token')
+ARGO_AUTH_BEARER_TOKEN=$(echo $ARGO_REPONSE | jq -r '.token')
 
-jq -n --arg ARGO_SERVICE_ACCOUNT_TOKEN "$ARGO_SERVICE_ACCOUNT_TOKEN" '{"argo_token":$ARGO_SERVICE_ACCOUNT_TOKEN}'
+ARGO_REPONSE=$(curl -k --fail --location --request POST "$ARGO_HOSTNAME/api/v1/account/kerberus-dashboard/token" \
+--header "Authorization: Bearer $ARGO_AUTH_BEARER_TOKEN") || \
+(echo "Fail to retreive kerberus-dashboard service token. Please check if kerberus-dashboard service account is present on ArgoCD" >&2; exit 1)
+
+ARGO_SERVICE_ACCOUNT_TOKEN=$(echo $ARGO_REPONSE | jq -r '.token')
+
+jq -n --arg ARGO_SERVICE_ACCOUNT_TOKEN "$ARGO_SERVICE_ACCOUNT_TOKEN" --arg ID "$RANDOM" '{"id": $ID, "argo_token":$ARGO_SERVICE_ACCOUNT_TOKEN}'
